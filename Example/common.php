ba207c2121a22d27d5d9d8bef8dbda7e99b7571a
ProWON <pro_won45@naver.com>
2022-04-19 13:48:32 +0900

    added UseLocalTimeYN

--- Example/common.php
+++ Example/common.php
@@ -18,7 +18,7 @@
 
     // 통신방식 기본은 CURL , curl 사용에 문제가 있을경우 STREAM 사용가능.
     // STREAM 사용시에는 php.ini의 allow_url_fopen = on 으로 설정해야함.
-    define('LINKHUB_COMM_MODE','STREAM');
+    define('LINKHUB_COMM_MODE','CURL');
 
     $KakaocertService = new KakaocertService($LinkID, $SecretKey);
 
@@ -27,5 +27,8 @@
 
     // 카카오써트 API 서비스 고정 IP 사용여부, 기본값(false)
     $KakaocertService->UseStaticIP(false);
+
+    // 로컬서버 시간 사용 여부 true(기본값) - 사용, false(미사용)
+    $KakaocertService->UseLocalTimeYN(true);
 
 ?>
